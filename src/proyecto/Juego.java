30f5369db6390496c80e0ba4843ae326cc2196f3
Oscar david Cuaical <cuaical.oscar@correounivalle.edu.co>
2023-04-19 21:57:18 -0500

    Se da inicio a la creacion de la ventana del juego, donde agrgamos los botones e implementamos los eventos

---
+++ src/proyecto/Juego.java
@@ -0,0 +1,127 @@
+
+package proyecto;
+
+import javax.swing.JOptionPane;
+
+
+public class Juego extends javax.swing.JFrame {
+
+    /**
+     * Creates new form Juego
+     */
+    public Juego() {
+        initComponents();
+    }
+
+     
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        Jugar = new javax.swing.JButton();
+        Instruccion = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(0, 153, 153));
+        setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
+        setFont(new java.awt.Font("Calibri", 0, 10)); // NOI18N
+
+        Jugar.setBackground(new java.awt.Color(0, 255, 255));
+        Jugar.setFont(new java.awt.Font("Dubai Light", 1, 14)); // NOI18N
+        Jugar.setText("Jugar");
+        Jugar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JugarActionPerformed(evt);
+            }
+        });
+
+        Instruccion.setBackground(new java.awt.Color(102, 255, 0));
+        Instruccion.setFont(new java.awt.Font("Dubai Light", 1, 14)); // NOI18N
+        Instruccion.setText("Instrucciones");
+        Instruccion.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                InstruccionActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(155, 155, 155)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(Instruccion)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(Jugar)
+                        .addGap(25, 25, 25)))
+                .addContainerGap(141, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(143, 143, 143)
+                .addComponent(Jugar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(Instruccion)
+                .addContainerGap(71, Short.MAX_VALUE))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void JugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JugarActionPerformed
+        boton2 newframe= new boton2();
+        newframe.setVisible(true);
+        this.dispose();//para que no se acumulen las ventanas 
+    }//GEN-LAST:event_JugarActionPerformed
+
+    private void InstruccionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_InstruccionActionPerformed
+        // TODO add your handling code here:
+        JOptionPane.showMessageDialog(null, "El juego mostrara una imagen "
+                + "seguida de una palabra la cual habra que completar con una vocal,"
+                + " preciona (ACEPTAR) para regresar a la ventana prinicipal");
+    }//GEN-LAST:event_InstruccionActionPerformed
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Juego.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Juego().setVisible(true);
+            }
+        });
+    }
+    
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Instruccion;
+    private javax.swing.JButton Jugar;
+    // End of variables declaration//GEN-END:variables
+}
+
