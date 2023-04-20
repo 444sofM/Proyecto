24c5539e98daf3c82b0e70502c9df5dc0489e1de
Oscar David Cuaical <cuaical.oscar@correounivalle.edu.co>
2023-04-19 22:16:55 -0500

    Creo nuevos botones para las vocales y tambien inicio los eventos de los mismos

--- src/proyecto/boton2.java
+++ src/proyecto/boton2.java
@@ -84,7 +84,7 @@ public class boton2 extends javax.swing.JFrame {
                         .addComponent(ingresanombre, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(34, 34, 34)))
                 .addComponent(escribirnombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 85, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 100, Short.MAX_VALUE)
                 .addComponent(iniciojuego)
                 .addGap(42, 42, 42))
         );
